02f76c3b191ca4ae91ef845b29c8b80e3dc16495
Lars van der Laan <Vanderlaanlars@yahoo.com>
2023-01-03 14:06:30 -0800

    Update scenario2.sh

--- scenario2.sh
+++ scenario2.sh
@@ -1,11 +1,11 @@
 #!/bin/usr/env bash
 export R_LIBS=~/Rlibs2
 export R_LIBS_USER=~/Rlibs2
-for S in 4 4 4 4
+for S in 4 11
 do
   for ss in 1000 2000 5000 10000
   do
-    sbatch  --export=s=$s,ss=ss ~/calibration/scenario2.sbatch
+    sbatch  --export=s=$s,ss=ss ~/calibration/scenario.sbatch
   done
 done
 
